88c9cb9df5f2a05ae9d81065d41f50fb532e4ffa
Jakub <jwf.adamczyk@gmail.com>
2026-01-19 23:21:51 +0100

    Reduced font size in `SfmlOutput` from 32 to 30 for improved UI scaling.

--- Erg/Program.cs
+++ Erg/Program.cs
@@ -1,7 +1,7 @@
 ﻿using Erg.Core.Game;
 using Erg.Platforms.Sfml;
 
-var output = new SfmlOutput(80, 25, "Erg", "Assets/CascadiaCode-Light.ttf", 32);
+var output = new SfmlOutput(80, 25, "Erg", "Assets/CascadiaCode-Light.ttf", 30);
 var input = new SfmlInput();
 var game = new Game(output, input);
 
